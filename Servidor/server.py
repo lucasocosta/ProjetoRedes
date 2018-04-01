af8b4f2aa8594fb647bd36274785ce1fc95fa031
Lucas Oliveira Costa <lucasocosta@hotmail.com>
2018-04-01 19:12:24 -0300

    Add files via upload

---
+++ Servidor/server.py
@@ -0,0 +1,69 @@
+import socket
+import sys
+import glob, os
+from thread import *
+
+os.chdir("content/")
+HOST = ''   # Symbolic name meaning all available interfaces
+PORT = 8880 # Arbitrary non-privileged port
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print 'Socket created'
+
+#Bind socket to local host and port
+try:
+    s.bind((HOST, PORT))
+except socket.error , msg:
+    print 'Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
+    sys.exit()
+
+print 'Socket bind complete'
+
+#Start listening on socket
+s.listen(10)
+print 'Socket now listening'
+
+#Function for handling connections. This will be used to create threads
+def clientthread(conn):
+    #Sending message to connected client
+    #conn.send('Welcome to the server. Type something and hit enter\n') #send only takes string
+
+    #infinite loop so that function do not terminate and thread do not end.
+    while True:
+
+        #Receiving from client
+        data = conn.recv(1024)
+        Lista=data.split()
+        print(Lista)
+        if "GET" in Lista:
+            arq=str(Lista[1])
+            reply=''
+            if(arq=='/'):
+                for file in glob.glob("*.html"):
+                    reply = file +'\n'+reply
+            else:
+                arq=arq[1:]
+                for file in glob.glob("*.html"):
+                    if(file==arq):
+                        reply = '200 ' + file
+                        break
+                if '200' not in reply:
+                    reply = '404'
+        if not data:
+            break
+
+        conn.sendall(reply)
+    Lista=[]
+    #came out of loop
+    conn.close()
+
+#now keep talking with the client
+while 1:
+    #wait to accept a connection - blocking call
+    conn, addr = s.accept()
+    print 'Connected with ' + addr[0] + ':' + str(addr[1])
+
+    #start new thread takes 1st argument as a function name to be run, second is the tuple of arguments to the function.
+    start_new_thread(clientthread ,(conn,))
+
+s.close()
---
+++ client.py
@@ -0,0 +1,48 @@
+#Socket client example in python
+
+import socket   #for sockets
+import sys  #for exit
+
+#create an INET, STREAMing socket
+try:
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+except socket.error:
+    print 'Failed to create socket'
+    sys.exit()
+
+print 'Socket Created'
+
+host = 'localhost';
+port = 8880;
+
+try:
+    remote_ip = socket.gethostbyname( host )
+
+except socket.gaierror:
+    #could not resolve
+    print 'Hostname could not be resolved. Exiting'
+    sys.exit()
+
+#Connect to remote server
+s.connect((remote_ip , port))
+
+print 'Socket Connected to ' + host + ' on ip ' + remote_ip
+
+texto=raw_input("url: ")
+#Send some data to remote server
+message = "GET /"+texto+" HTTP/1.1\r\nUser-Agent: Mozilla/4.0 (compatible; MSIE5.01; Windows NT)\r\nHost: "+host+"\r\nAccept-Language: en-us\r\nAccept-Encoding: gzip, deflate\r\nConnection: Keep-Alive"
+print(message)
+try :
+    #Set the whole string
+    s.sendall(message)
+except socket.error:
+    #Send failed
+    print 'Send failed'
+    sys.exit()
+
+print 'Message send successfully'
+
+#Now receive data
+reply = s.recv(4096)
+
+print reply
